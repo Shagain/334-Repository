23c0f19b480bfbb5b2dfe0e21a4b0bda1016f59d
Cyclone1070 <128883620+Cyclone1070@users.noreply.github.com>
2026-05-01 14:33:30 +1000

    added docker

---
+++ backend/Infrastructure/Data/Migrations/20260501041850_InitialCreate.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SmartParking.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "zones",
+                columns: table => new
+                {
+                    zone_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    capacity = table.Column<int>(type: "integer", nullable: false),
+                    price_per_hour = table.Column<double>(type: "double precision", nullable: false),
+                    max_duration = table.Column<int>(type: "integer", nullable: false),
+                    access_level = table.Column<string>(type: "text", nullable: false),
+                    zone_type = table.Column<string>(type: "text", nullable: false),
+                    geo_json = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_zones", x => x.zone_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "notifications",
+                columns: table => new
+                {
+                    notification_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    message = table.Column<string>(type: "text", nullable: false),
+                    channel = table.Column<string>(type: "text", nullable: false),
+                    sent_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_notifications", x => x.notification_id);
+                    table.ForeignKey(
+                        name: "fk_notifications_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehicles",
+                columns: table => new
+                {
+                    vehicle_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    license_plate = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_vehicles", x => x.vehicle_id);
+                    table.ForeignKey(
+                        name: "fk_vehicles_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "parking_spots",
+                columns: table => new
+                {
+                    spot_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    spot_number = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    zone_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_parking_spots", x => x.spot_id);
+                    table.ForeignKey(
+                        name: "fk_parking_spots_zones_zone_id",
+                        column: x => x.zone_id,
+                        principalTable: "zones",
+                        principalColumn: "zone_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bookings",
+                columns: table => new
+                {
+                    booking_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    start_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    spot_id = table.Column<int>(type: "integer", nullable: false),
+                    vehicle_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_bookings", x => x.booking_id);
+                    table.ForeignKey(
+                        name: "fk_bookings_parking_spots_spot_id",
+                        column: x => x.spot_id,
+                        principalTable: "parking_spots",
+                        principalColumn: "spot_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_bookings_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_bookings_vehicles_vehicle_id",
+                        column: x => x.vehicle_id,
+                        principalTable: "vehicles",
+                        principalColumn: "vehicle_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "parking_sessions",
+                columns: table => new
+                {
+                    session_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    start_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    status = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    spot_id = table.Column<int>(type: "integer", nullable: false),
+                    vehicle_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_parking_sessions", x => x.session_id);
+                    table.ForeignKey(
+                        name: "fk_parking_sessions_parking_spots_spot_id",
+                        column: x => x.spot_id,
+                        principalTable: "parking_spots",
+                        principalColumn: "spot_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_parking_sessions_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_parking_sessions_vehicles_vehicle_id",
+                        column: x => x.vehicle_id,
+                        principalTable: "vehicles",
+                        principalColumn: "vehicle_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "payments",
+                columns: table => new
+                {
+                    payment_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    amount = table.Column<double>(type: "double precision", nullable: false),
+                    method = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    paid_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    booking_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_payments", x => x.payment_id);
+                    table.ForeignKey(
+                        name: "fk_payments_bookings_booking_id",
+                        column: x => x.booking_id,
+                        principalTable: "bookings",
+                        principalColumn: "booking_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "violations",
+                columns: table => new
+                {
+                    violation_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    detected_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    session_id = table.Column<int>(type: "integer", nullable: true),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_violations", x => x.violation_id);
+                    table.ForeignKey(
+                        name: "fk_violations_parking_sessions_session_id",
+                        column: x => x.session_id,
+                        principalTable: "parking_sessions",
+                        principalColumn: "session_id");
+                    table.ForeignKey(
+                        name: "fk_violations_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_spot_id",
+                table: "bookings",
+                column: "spot_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_user_id",
+                table: "bookings",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_vehicle_id",
+                table: "bookings",
+                column: "vehicle_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_notifications_user_id",
+                table: "notifications",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_parking_sessions_spot_id",
+                table: "parking_sessions",
+                column: "spot_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_parking_sessions_user_id",
+                table: "parking_sessions",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_parking_sessions_vehicle_id",
+                table: "parking_sessions",
+                column: "vehicle_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_parking_spots_zone_id",
+                table: "parking_spots",
+                column: "zone_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_payments_booking_id",
+                table: "payments",
+                column: "booking_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_vehicles_license_plate",
+                table: "vehicles",
+                column: "license_plate",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_vehicles_user_id",
+                table: "vehicles",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_violations_session_id",
+                table: "violations",
+                column: "session_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_violations_user_id",
+                table: "violations",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "notifications");
+
+            migrationBuilder.DropTable(
+                name: "payments");
+
+            migrationBuilder.DropTable(
+                name: "violations");
+
+            migrationBuilder.DropTable(
+                name: "bookings");
+
+            migrationBuilder.DropTable(
+                name: "parking_sessions");
+
+            migrationBuilder.DropTable(
+                name: "parking_spots");
+
+            migrationBuilder.DropTable(
+                name: "vehicles");
+
+            migrationBuilder.DropTable(
+                name: "zones");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
